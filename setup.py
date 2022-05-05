809b6cb1143df3b9a2717d4148ad14cbf96e4d3f
Meghanto Majumder <meghanto20@gmail.com>
2022-05-05 13:24:18 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+setup(
+    name='pywrap',
+    packages=find_packages(),
+    version='0.0.1',
+    description='My first Python library',
+    author='meghanto',
+    license='MIT',
+    install_requires=['cppimport','pybind11']
+)
